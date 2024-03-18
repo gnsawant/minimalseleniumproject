8e455768a788625a0b94959b95157cd0fda5ebfb
gnsawant <107615254+gnsawant@users.noreply.github.com>
2024-03-18 18:44:04 +0530

    Update LoginBasicTest.java

--- src/test/java/tests/LoginBasicTest.java
+++ src/test/java/tests/LoginBasicTest.java
@@ -19,7 +19,7 @@ import org.testng.annotations.*;
  */
 public class LoginBasicTest {
 	//IMPORTANT: Please download a Chrome driver and set this variable to the full path to the file
-	private final static String CHROME_DRIVER_FULL_PATH = "D:\Geetanjali\2023_24\CSE_SEPM\LabPlanandExpt\chromedriver_win32";
+	private final static String CHROME_DRIVER_FULL_PATH = "D:/Geetanjali/2023_24/CSE_SEPM/LabPlanandExpt/chromedriver_win32";
 	//private final static String GECKO_DRIVER_FULL_PATH = "/Users/leonardolanni/Downloads/geckodriver";
 	private WebDriver driver;
 
